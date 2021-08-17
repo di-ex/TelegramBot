25a474a0834a04219179d088947a3b1c93db21f5
di-ex <85092768+di-ex@users.noreply.github.com>
2021-08-17 20:50:31 +0700

    18.6.1

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '1941435761:AAEr0NzxwTdLxsILkUZiNSpHAQwwWEri-uo'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'японская_йена': 'JPY',
+    'фунт_стерлингов': 'GBP',
+    'чешская_крона': 'CZK',
+}
